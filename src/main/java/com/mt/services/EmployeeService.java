61eb9ec1c31107382293248b50a1edefcd415cfa
saikrishnaMT15 <saikrishnamallugari@gmail.com>
2020-04-17 23:11:47 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -23,7 +23,7 @@ public class EmployeeService {
 
 		JSONObject js = new JSONObject();
 		js.put("Name", "Sai krishna");
-		js.put("Calling Name", "Sai");
+		js.put("Calling Name", "Saikrishna");
 		js.put("DOB", "05-Apr-1993");
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
 		js.put("Places he like", "His native place");
